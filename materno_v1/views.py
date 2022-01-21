0885a9a68cb5a50e210de95b3b837537fe842cbd
Laura <laura.svilla21@gmail.com>
2022-01-20 19:59:54 -0500

    Todo esto es un proyecto de prueba

--- materno_v1/views.py
+++ materno_v1/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'materno_v1/dashboard.html')
+
+def profile(request):
+    return render(request, 'materno_v1/profile.html')
